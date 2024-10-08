3996addd85d5c9a9876af99cae664158481b47fa
jrrHernandez <123601048+jrrHernandez@users.noreply.github.com>
2024-10-08 11:46:45 -0500

    Update Comenzando con R.R

--- Introducción a R/Comenzando con R.R
+++ Introducción a R/Comenzando con R.R
@@ -10,3 +10,40 @@ install.packages(c("AER", "car", "censReg","dplyr", "dummies", "dynlm",
                    "stargazer", "survival", "systemfit", "truncreg", "tseries", 
                    "ulcra", "xtable", "vars", "WDI", "wooldridge", "xts", "zoo"))
 
+#obtener directorio de trabajo y seleccionar uno
+getwd()
+setwd("C:/Users/richa/Music/Este pc/GIthub/Ejercicios-R/Introducción a R")
+
+########funciones Aritmericas#############
+#valor absoluto
+x<- -4
+abs(x)
+#raiz cuadrada
+sqrt(abs(x))
+#exponencial
+exp(x)
+#logaritmo
+log(abs(x))
+#logaritmo de a en base b
+a<- 10
+b<- 4
+log(a,b)
+#redondear un numero a con b digitos
+round(a,b)
+#factorial
+factorial(b)
+#coeficiente binomial
+choose(a,b)
+###########Incluir elementos dentro de elementos
+x <- 3 + exp(a)
+y <- x^2
+
+
+#####vectores
+x <- c(1,2,3,4,5)
+y <- x + 1
+z <- x+y
+w <-z*y
+exp(w)
+
+#funciones para 

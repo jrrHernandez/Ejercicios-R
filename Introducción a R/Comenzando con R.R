0930bb06f822ea47a8bac4e6940e03e3b3a13f40
jrrHernandez <123601048+jrrHernandez@users.noreply.github.com>
2024-10-08 11:44:51 -0500

    Create Comenzando con R.R

---
+++ Introducción a R/Comenzando con R.R
@@ -0,0 +1,12 @@
+#getting started with r
+#run a large code wirh ;
+#correr todo el script con ctr + a
+#si no queremos descargar una libreria
+libreria :: funcion()
+#descargar librerias importantes
+install.packages(c("AER", "car", "censReg","dplyr", "dummies", "dynlm", 
+                   "effects", "ggplot2", "lmtest", "maps", "mfx", "orcutt", "plm",
+                   "quantmod", "sandwich", "quantreg", "rio", "rmarkdown", "sample selection",
+                   "stargazer", "survival", "systemfit", "truncreg", "tseries", 
+                   "ulcra", "xtable", "vars", "WDI", "wooldridge", "xts", "zoo"))
+
